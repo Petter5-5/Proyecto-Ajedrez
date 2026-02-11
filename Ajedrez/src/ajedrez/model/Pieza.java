67f4a9656a092b1c5ad4b91b65eafa5ad38ccca6
Petter5-5 <yeraytrejosanchez@gmail.com>
2026-02-11 17:48:29 +0100

    Avanze en la clase Pieza

--- Ajedrez/src/ajedrez/model/Pieza.java
+++ Ajedrez/src/ajedrez/model/Pieza.java
@@ -1,9 +1,57 @@
 package ajedrez.model;
 
-/**
- *
- * @author ytresan889
- */
-public class Pieza {
+import java.util.List;
+import java.util.ArrayList;
+
+public abstract class Pieza 
+{
+    enum Color
+    {
+        BLANCO,
+        NEGRO
+    }
     
+    enum Tipo
+    {
+        REY,
+        REINA,
+        TORRE,
+        ALFIL,
+        CABALLO,
+        PEON
+    }
+    
+    private Color color;
+    private Tipo tipo;
+    private int fila;
+    private int columna;
+    private boolean aMovido;
+    
+    private static String[][] SIMBOLOS = {
+        { "♔", "♚" },
+        { "♕", "♛" },
+        { "♖", "♜" },
+        { "♗", "♝" },
+        { "♘", "♞" },
+        { "♙", "♟" }
+    };
+    
+    public Pieza(Color color, Tipo tipo, int fila, int columna)
+    {
+        this.color = color;
+        this.tipo = tipo;
+        this.fila = fila;
+        this.columna = columna;
+    }
+    
+    
+    
+    public abstract List<Movimiento> movimientosValidos(Tablero tablero);
+    
+    public String getSimbolo(Color color, Tipo tipo)
+    {
+        return SIMBOLOS[color.ordinal()][tipo.ordinal()];
+    }
+    
+    public 
 }
